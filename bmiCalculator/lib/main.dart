3a596ba3149ad79c13b3c6bf01d99451e935a83c
= <aayushdongre4@gmail.com>
2020-06-11 23:25:59 +0530

    Initial commit

---
+++ bmiCalculator/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
